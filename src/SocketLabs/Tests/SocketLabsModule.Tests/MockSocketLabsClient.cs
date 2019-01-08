b0e4a5fab17bb16d822714036fbea03b09dadf13
msoler8785@gmail.com <msoler8785@gmail.com>
2019-01-08 14:06:43 -0500

    Add some tests

---
+++ src/SocketLabs/Tests/SocketLabsModule.Tests/MockSocketLabsClient.cs
@@ -0,0 +1,32 @@
+﻿using SocketLabs.InjectionApi;
+using SocketLabs.InjectionApi.Message;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketLabsModule.Tests
+{
+    public sealed class MockSocketLabsClient : ISocketLabsClient
+    {
+        public SendResponse Send(IBasicMessage message)
+        {
+            return new SendResponse() { Result = SendResult.Success };
+        }
+
+        public SendResponse Send(IBulkMessage message)
+        {
+            return new SendResponse() { Result = SendResult.Success };
+        }
+
+        public Task<SendResponse> SendAsync(IBasicMessage message)
+        {
+            return Task.Run(() => new SendResponse() { Result = SendResult.Success });
+        }
+
+        public Task<SendResponse> SendAsync(IBulkMessage message)
+        {
+            return Task.Run(() => new SendResponse() { Result = SendResult.Success });
+        }
+    }
+}
