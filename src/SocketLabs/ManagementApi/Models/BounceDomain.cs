2e12750dae5892bcf4e4f5ec8f6cb5ccb3c6405c
Matt Soler <msoler8785@gmail.com>
2020-01-22 08:28:08 -0500

    Add management api sub module.

---
+++ src/SocketLabs/ManagementApi/Models/BounceDomain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementApi.Models
+{
+    public class BounceDomain
+    {
+        public string Domain { get; set; }
+        public bool IsDefault { get; set; }
+    }
+
+}
---
+++ src/SocketLabs/ManagementApi/PowerShell/Commands/GetBounceDomain.cs
@@ -0,0 +1,73 @@
+﻿using ManagementApi.Models;
+using SocketLabsModule.Common;
+using SocketLabsModule.Common.Services;
+using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Net.Http;
+using System.Text;
+
+namespace ManagementApi.PowerShell.Commands
+{
+    /// <summary>
+    /// Implementation for the Out-SocketLabs command.
+    /// </summary>
+    [Cmdlet(
+        VerbsCommon.Get,
+        "BounceDomain",
+        SupportsShouldProcess = true,
+        DefaultParameterSetName = "Default",
+        HelpUri = "https://github.com/socketlabs/socketlabs-powershell/blob/master/README.md"
+        )]
+    public class GetBounceDomain : CommandBase
+    {
+        private const string BASE_URL = "https://api.socketlabs.com/v1/servers";
+
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "Default")]
+        public int ServerId { get; set; }
+
+        [Parameter(Mandatory = false, Position = 1, ParameterSetName = "Default")]
+        public string Domain { get; set; }
+
+        [Parameter(Mandatory = true, Position = 2, ParameterSetName = "Default")]
+        public string ApiKey { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+        }
+
+        protected override void EndProcessing()
+        {
+            base.EndProcessing();
+        }
+
+        protected override void ProcessRecord()
+        {
+            IRestProvider rest = new RestProvider(ApiKey);
+            try
+            {
+                var results = rest.GetAsync<IEnumerable<BounceDomain>>($"{BASE_URL}/{ServerId}/bounce").Result;
+
+                foreach (var item in results)
+                {
+                    base.WriteObject(item);
+                }
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var inner in ex.InnerExceptions)
+                {
+                    base.WriteError(new ErrorRecord(inner, String.Empty, ErrorCategory.WriteError, null));
+                }
+            }
+
+            base.ProcessRecord();
+        }
+
+        protected override void StopProcessing()
+        {
+            base.StopProcessing();
+        }
+    }
+}
